eeb4848b83b008c2bacc8830b38b529c83caa1a0
gowtham-b-14 <gowtham200014@gmail.com>
2021-04-17 18:36:25 +0530

    first commit

---
+++ app/src/main/java/com/example/login/ui/login/content.java
@@ -0,0 +1,15 @@
+package com.example.login.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.login.R;
+
+public class content extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content);
+    }
+}
